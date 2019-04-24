9c67ea2f43952dc0db55f2572147e292f445d014
Gilbert François Duivesteijn <info@gilbertfrancois.com>
2019-04-24 18:17:25 +0200

    Adds image classifier code for fashion mnist

---
+++ image_classifier/mnist_gluon.py
@@ -0,0 +1,137 @@
+import logging
+import time
+
+import matplotlib.pyplot as plt
+import mxnet as mx
+import numpy as np
+from mxnet import gluon, init, autograd
+from mxnet.gluon.data.vision import transforms
+from mxnet.gluon.nn import BatchNorm
+from mxnet.gluon.nn import Conv2D
+from mxnet.gluon.nn import Dense
+from mxnet.gluon.nn import Dropout
+from mxnet.gluon.nn import Flatten
+from mxnet.gluon.nn import MaxPool2D
+from mxnet.gluon.nn import Sequential
+
+# Set logging to see some output during training
+logging.getLogger().setLevel(logging.DEBUG)
+
+# Fix the seed
+mx.random.seed(42)
+
+# Set the compute context, GPU is available otherwise CPU
+mx_ctx = mx.gpu() if mx.test_utils.list_gpus() else mx.cpu()
+
+# %%
+# -- Constants
+
+EPOCHS = 3
+BATCH_SIZE = 256
+
+# %%
+# -- Get some data to train on
+
+mnist_train = gluon.data.vision.datasets.FashionMNIST(train=True)
+mnist_valid = gluon.data.vision.datasets.FashionMNIST(train=False)
+
+transformer = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(0.286, 0.353)
+])
+
+# %%
+# Create a data iterator that feeds batches
+
+train_data = gluon.data.DataLoader(mnist_train.transform_first(transformer),
+                                   batch_size=BATCH_SIZE,
+                                   shuffle=True,
+                                   num_workers=4)
+
+eval_data = gluon.data.DataLoader(mnist_valid.transform_first(transformer),
+                                  batch_size=BATCH_SIZE,
+                                  num_workers=4
+                                  )
+
+# %%
+# -- Define the model
+
+net = Sequential()
+with net.name_scope():
+    net.add(
+        # layer 1
+        Conv2D(channels=32, kernel_size=(5, 5), padding=(5 // 2, 5 // 2), activation='relu'),
+        BatchNorm(axis=1, momentum=0.995, epsilon=0.001),
+        # layer 2
+        Conv2D(channels=32, kernel_size=(5, 5), padding=(5 // 2, 5 // 2), activation='relu'),
+        BatchNorm(axis=1, momentum=0.995, epsilon=0.001),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        # layer 3
+        Conv2D(channels=64, kernel_size=(3, 3), padding=(3 // 2, 3 // 2), activation='relu'),
+        BatchNorm(axis=1, momentum=0.995, epsilon=0.001),
+        # layer 4
+        Conv2D(channels=64, kernel_size=(3, 3), padding=(3 // 2, 3 // 2), activation='relu'),
+        BatchNorm(axis=1, momentum=0.995, epsilon=0.001),
+        MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+        # layer 5
+        Flatten(),
+        Dense(1024, activation='relu'),
+        BatchNorm(axis=1, momentum=0.995, epsilon=0.001),
+        # layer 6
+        Dropout(0.3),
+        Dense(10)
+    )
+
+# %%
+# -- Initialize parameters
+
+net.initialize(init=init.Xavier(), ctx=mx_ctx)
+
+# %%
+# -- Define loss function and optimizer
+
+loss_fn = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'Adam', {'learning_rate': 0.001})
+
+
+# %%%
+
+def acc(output, label):
+    return (output.argmax(axis=1) == label.astype('float32')).mean().asscalar()
+
+
+# %%
+# -- Train
+
+t0 = time.time()
+
+for epoch in range(EPOCHS):
+    train_loss = 0
+    train_acc = 0
+    eval_acc = 0
+
+    for data, label in train_data:
+        data = data.as_in_context(mx_ctx)
+        label = label.as_in_context(mx_ctx)
+        with autograd.record():
+            output = net(data)
+            loss = loss_fn(output, label)
+        loss.backward()
+        trainer.step(batch_size=BATCH_SIZE)
+        train_loss += loss.mean().asscalar()
+        train_acc += acc(output, label)
+
+    for data, label in eval_data:
+        data = data.as_in_context(mx_ctx)
+        label = label.as_in_context(mx_ctx)
+        eval_acc += acc(net(data), label)
+
+    print("epoch {}, loss {:0.3f}, acc {:0.3f}, val_acc {:0.3f}".format(
+        epoch, train_loss / len(train_data), train_acc / len(train_data), eval_acc / len(eval_data)))
+
+print("Elapsed time {:02f} seconds".format(time.time() - t0))
+
+
+# %%
+net.save_parameters('fashion_mnist_gluon.params')
+# %%
---
+++ image_classifier/mnist_module.py
@@ -0,0 +1,120 @@
+import logging
+import time
+
+import mxnet as mx
+import numpy as np
+from mxnet import nd, gluon
+
+# Set logging to see some output during training
+logging.getLogger().setLevel(logging.DEBUG)
+
+# Fix the seed
+mx.random.seed(42)
+
+# Set the compute context, GPU is available otherwise CPU
+mx_ctx = mx.gpu() if mx.test_utils.list_gpus() else mx.cpu()
+
+# %%
+# -- Constants
+
+EPOCHS = 3
+BATCH_SIZE = 256
+
+
+# %%
+# -- Transformation helper functions (in Gluon we use Transformer)
+
+def to_tensor(X):
+    X = X.astype(np.float32) / 255.0
+    X = nd.moveaxis(X, 3, 1)
+    return X
+
+
+def normalize(X, mean_, std_):
+    assert std_ != 0
+    return (X - mean_) / nd.power(std_, 2)
+
+
+# %%
+# -- Get some data to train on
+
+# Get data
+mnist_train = gluon.data.vision.datasets.FashionMNIST(train=True)
+mnist_eval = gluon.data.vision.datasets.FashionMNIST(train=False)
+
+X_train = mnist_train._data
+y_train = mnist_train._label
+
+X_eval = mnist_eval._data
+y_eval = mnist_eval._label
+
+# To Tensor
+X_train = to_tensor(X_train)
+X_eval = to_tensor(X_eval)
+
+# Normalize
+X_train = normalize(X_train, 0.286, 0.353)
+X_eval = normalize(X_eval, 0.286, 0.353)
+
+# %%
+# Create a data iterator that feeds batches
+
+train_iter = mx.io.NDArrayIter(X_train, y_train, batch_size=BATCH_SIZE, shuffle=True,  label_name="softmax_label")
+eval_iter  = mx.io.NDArrayIter(X_eval,  y_eval,  batch_size=BATCH_SIZE, shuffle=False, label_name="softmax_label")
+
+# %%
+# -- Define the model
+#    Note that the loss function is implicitly given by the layer "SoftmaxOutput"
+
+# placeholders for input and output data
+data = mx.sym.Variable("data")
+label = mx.sym.Variable("softmax_label")
+# layer 1
+net = mx.sym.Convolution(data, kernel=(5, 5), pad=(5 // 2, 5 // 2), num_filter=32)
+net = mx.sym.Activation(net, act_type="relu")
+net = mx.sym.BatchNorm(net, momentum=0.995, eps=0.001)
+# layer 2
+net = mx.sym.Convolution(net, kernel=(5, 5), pad=(5 // 2, 5 // 2), num_filter=32)
+net = mx.sym.Activation(net, act_type="relu")
+net = mx.sym.BatchNorm(net, momentum=0.995, eps=0.001)
+net = mx.sym.Pooling(net, kernel=(2, 2), pool_type="max", stride=(2, 2))
+# layer 3
+net = mx.sym.Convolution(net, kernel=(3, 3), pad=(3 // 2, 3 // 2), num_filter=64)
+net = mx.sym.Activation(net, act_type="relu")
+net = mx.sym.BatchNorm(net, momentum=0.995, eps=0.001)
+# layer 4
+net = mx.sym.Convolution(net, kernel=(3, 3), pad=(3 // 2, 3 // 2), num_filter=64)
+net = mx.sym.Activation(net, act_type="relu")
+net = mx.sym.BatchNorm(net, momentum=0.995, eps=0.001)
+net = mx.sym.Pooling(net, kernel=(2, 2), pool_type="max", stride=(2, 2))
+# layer 5
+net = mx.sym.Flatten(net)
+net = mx.sym.FullyConnected(net, num_hidden=1024)
+net = mx.sym.Activation(net, act_type="relu")
+net = mx.sym.BatchNorm(net, momentum=0.995, eps=0.001)
+# layer 6
+net = mx.sym.Dropout(net, p=0.3)
+net = mx.sym.FullyConnected(net, num_hidden=10)
+net = mx.sym.SoftmaxOutput(net, label=label, name="softmax")
+
+model = mx.mod.Module(symbol=net, data_names=['data'], label_names=['softmax_label'], context=mx_ctx)
+
+# %%
+# -- Train
+#    Note that the optimizer is given as parameter in the fit() function as an argument.
+
+t0 = time.time()
+
+model.fit(train_iter, eval_iter,
+          num_epoch=EPOCHS,
+          optimizer="adam",
+          optimizer_params={"learning_rate": 0.001},
+          eval_metric="acc",
+          batch_end_callback=mx.callback.Speedometer(BATCH_SIZE)
+          )
+
+print("Elapsed time: {:0.2f} seconds".format(time.time() - t0))
+
+# %%
+model.save_checkpoint("fashion_mnist_module", EPOCHS)
+model.save_params("fashion_mnist_module.params")
